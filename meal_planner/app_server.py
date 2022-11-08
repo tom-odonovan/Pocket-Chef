73b38825fc10eb1bfb398487e56db62b9a3155e5
Tom O'Donovan <tom.odonovan01@gmail.com>
2022-11-08 20:17:26 +1100

    heroku push

--- meal_planner/app_server.py
+++ meal_planner/app_server.py
@@ -8,6 +8,7 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 
+
 @app.route('/')
 def index():
     return render_template('signup.html')
@@ -47,6 +48,7 @@ def signup_action():
         print(f'Welcome {given_name}')
         response = redirect('/login')
         session['user_id'] = given_name
+
         return response
 
 
@@ -82,11 +84,13 @@ def login_action():
         return render_template('login.html', error=error)
 
 
+
 @app.route('/logout')
 def logout():
     response = redirect('/home')
     session['user_id'] = ''
     return response
+
 
 
 @app.route('/home')
@@ -96,21 +100,52 @@ def home():
     return render_template('home.html', user=session_id)
 
 
+
 @app.route('/results', methods=['POST', 'GET'])
 def results():
+    session_id = session.get('user_id', 'Unknown')
     search = request.form['search']
 
     response = requests.get(
-        f"https://api.spoonacular.com/recipes/complexSearch?query={search}&apiKey=65ad0a272a4d414d8a1be12d6c839a0f"
-    )
+        f"https://api.spoonacular.com/recipes/complexSearch?query={search}&apiKey=55b5b8694b354c009c8b2c8939e1683b"
+    ) 
+    # response_2 = requests.get(
+    #     f"https://api.spoonacular.com/recipes/informationBulk?ids=75523&apiKey=55b5b8694b354c009c8b2c8939e1683b"
+    # )
+    # data_2 = response_2.json()
+    # print(data_2)
+
+    # response_2 = requests.get(
+    #     f"https://api.spoonacular.com/recipes/{632573}/information?apiKey=55b5b8694b354c009c8b2c8939e1683b"
+    # )
+    # data_2 = response_2.json()
+    # recipe_dict = dict(data_2)
+    # print(recipe_dict)
 
     data = response.json()
+
     if data['totalResults'] == 0:
-        return render_template('no_results.html', search=search)
+        return render_template('no_results.html', user=session_id, search=search)
     else:
         search_results = data['results']
-        print(search_results)
-        return render_template('results.html', search=search, search_results=search_results)
+        # print(search_results)
+        return render_template('results.html', user=session_id, search=search, search_results=search_results)
+
+
+
+@app.route('/recipe', methods=['POST', 'GET'])
+def recipe():
+    session_id = session.get('user_id', 'Unknown')
+    recipe_id = request.args.get('id')
+    response = requests.get(
+        f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey=55b5b8694b354c009c8b2c8939e1683b"
+    )
+    data = response.json()
+    recipe_dict = dict(data)
+    print(recipe_dict)
+
+    return render_template('recipe.html', user=session_id, recipe_dict=recipe_dict)
+
 
 
 # Run the server
